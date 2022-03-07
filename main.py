b03ec0ce84a6ae2568d704ede3a57dc36572d738
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-03-07 08:03:35 +0300

    написал тесты

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import human
+import house
 
+if __name__=='__main__':
+    human.Human.default_info()
+    vasya = human.Human('Vasya',23)
+    vasya.info()
+
+    sm_house = house.Small_House(12_000)
+
+    vasya.buy_house(sm_house,5)
+    vasya.earn_money(15_000)
+    vasya.buy_house(sm_house,5)
+    vasya.info()
